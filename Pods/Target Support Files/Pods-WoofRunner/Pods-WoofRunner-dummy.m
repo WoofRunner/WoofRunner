e214059cb7a06604eeee6bc8e72fb2ddd0f28591
Bili Xu <xubili93@gmail.com>
2017-03-10 14:03:51 +0800

    Add Podfile and Cocoapod support

---
+++ Pods/Target Support Files/Pods-WoofRunner/Pods-WoofRunner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WoofRunner : NSObject
+@end
+@implementation PodsDummy_Pods_WoofRunner
+@end
---
+++ Pods/Target Support Files/Pods-WoofRunnerTests/Pods-WoofRunnerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WoofRunnerTests : NSObject
+@end
+@implementation PodsDummy_Pods_WoofRunnerTests
+@end
