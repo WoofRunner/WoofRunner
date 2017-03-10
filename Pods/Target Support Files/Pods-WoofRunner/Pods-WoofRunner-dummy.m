ba5256be3114acae6140d7aaa9d706882d294843
Bili Xu <xubili93@gmail.com>
2017-03-10 14:17:40 +0800

    Remove Cocoapods from project repository

--- Pods/Target Support Files/Pods-WoofRunner/Pods-WoofRunner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WoofRunner : NSObject
-@end
-@implementation PodsDummy_Pods_WoofRunner
-@end
--- Pods/Target Support Files/Pods-WoofRunnerTests/Pods-WoofRunnerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WoofRunnerTests : NSObject
-@end
-@implementation PodsDummy_Pods_WoofRunnerTests
-@end
