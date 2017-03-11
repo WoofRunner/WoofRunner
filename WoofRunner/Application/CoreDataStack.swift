e9d2cb8b850b03eab6a79c94d33c3ee7686027f1
Bili Xu <xubili93@gmail.com>
2017-03-11 23:02:25 +0800

    Implement CoreDataStack

---
+++ WoofRunner/Application/CoreDataStack.swift
@@ -0,0 +1,62 @@
+//
+//  CoreDataStack.swift
+//  WoofRunner
+//
+//  Created by Xu Bili on 3/11/2017.
+//  Copyright © 2017 WoofRunner. All rights reserved.
+//
+
+import CoreData
+
+/*
+ Essential methods needed to implement data persistance via CoreData.
+
+ Instead of initializing new project and leaving all the code in AppDelegate,
+ we move it into a separate class that can be reused in different projects.
+
+ Inspiration and code taken from:
+ https://blog.jayway.com/2016/08/12/whats-new-core-data-swift-3-0/
+ */
+public class CoreDataStack {
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "WoofRunner")
+        container.loadPersistentStores(completionHandler: {
+            (storeDescription, error) in
+
+            if let error = error {
+                fatalError("Unresolved error \(error)")
+            }
+
+        })
+
+        return container
+    }()
+
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(
+            for: .applicationSupportDirectory,
+            in: .userDomainMask
+        )
+
+        let documentsDirectory = paths[0]
+
+        return documentsDirectory
+    }
+
+    private func firstUserLaunch() -> Bool {
+        return !UserDefaults.standard.bool(forKey: "launchedBefore")
+    }
+
+}
