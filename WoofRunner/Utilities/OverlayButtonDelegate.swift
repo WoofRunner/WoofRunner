a74766659f83e03219e412660b85775ee7f5f680
See Loo Jane <muigek34@yahoo.com.sg>
2017-03-26 03:22:39 +0800

    Add stub for OverlayButtonDelegate

---
+++ WoofRunner/Utilities/OverlayButtonDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  OverlayButtonDelegate.swift
+//  WoofRunner
+//
+//  Created by See Loo Jane on 26/3/17.
+//  Copyright © 2017 WoofRunner. All rights reserved.
+//
+
+import Foundation
+
+protocol OverlayButtonDelegate {
+	
+}
