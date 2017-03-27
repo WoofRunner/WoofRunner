05485ef478ddbe59d750613a9cbd82aea8823874
Bili Xu <xubili93@gmail.com>
2017-03-27 18:47:16 +0800

    Move LMLoginViewModel to Models directory/group

--- WoofRunner/ViewControllers/LMLoginViewController.swift
+++ WoofRunner/ViewControllers/LMLoginViewController.swift
@@ -13,53 +13,6 @@ import FirebaseAuth
 import FacebookCore
 import FacebookLogin
 
-public class LMLoginViewModel {
-
-    public var facebookAuthed = Variable<Bool>(false)
-    public var firebaseAuthed = Variable<Bool>(false)
-    public var authFailure = Variable<Bool?>(nil)
-    public var isAuthed: Observable<Bool> {
-        return Observable.combineLatest(
-            facebookAuthed.asObservable(), firebaseAuthed.asObservable()
-        ) { (facebook, firebase) in
-            return facebook && firebase
-        }
-    }
-
-    private let osm = OnlineStorageManager.getInstance()
-
-    /// Authenticates with Facebook
-    public func authWithFacebook(viewController vc: UIViewController) {
-        if let accessToken = AccessToken.current {
-            self.facebookAuthed.value = true
-            self.authWithFirebase(token: accessToken.authenticationToken)
-            return
-        }
-
-        let loginManager = LoginManager()
-        loginManager.logIn([.publicProfile], viewController: vc) { loginResult in
-            switch loginResult {
-            case .failed:
-                print("Log in failed")
-            case .cancelled:
-                print("Log in cancelld")
-            case .success( _, _, let accessToken):
-                self.facebookAuthed.value = true
-                self.authWithFirebase(token: accessToken.authenticationToken)
-            }
-        }
-    }
-
-    /// Authenticates with Firebase
-    /// - Parameters:
-    ///     - token: Facebook token obtained from authentication
-    private func authWithFirebase(token: String) {
-        osm.auth(token: token)
-            .onSuccess { _ in self.firebaseAuthed.value = true }
-            .onFailure { _ in self.authFailure.value = true }
-    }
-
-}
 
 /**
  View controller for Facebook Login view to access the Online Level Marketplace.
