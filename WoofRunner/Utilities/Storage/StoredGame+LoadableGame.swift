fedcd32fbba2cd7ea43cc3df6aecd049e4918dcb
Bili Xu <xubili93@gmail.com>
2017-03-28 16:07:04 +0800

    Implement methods to turn StoredGame into a LoadableGame

--- WoofRunner/Utilities/Storage/StoredGame+LoadableGame.swift
+++ WoofRunner/Utilities/Storage/StoredGame+LoadableGame.swift
@@ -11,11 +11,41 @@ extension StoredGame: LoadableGame {
     /// Returns the stored platforms as a 2D array.
     /// - Returns: 2D array of platform types
     public func getPlatforms() -> [[Int]] {
+        let DEFAULT_VALUE = 0
+
+        var res = create2DArray(rows: Int(rows), cols: Int(columns), initialValue: DEFAULT_VALUE)
+        let storedPlatforms = self.value(forKey: "platforms") as! [StoredPlatform]
+
+        for platform in storedPlatforms {
+            let col = Int(platform.positionX)
+            let row = Int(platform.positionY)
+
+            res[row][col] = Int(platform.type!)!
+        }
+
+        return res
     }
 
     /// Returns the stored obstacles of a game as a 2D array.
     /// - Returns: 2D array of obstacle types
     public func getObstacles() -> [[Int]] {
+        let DEFAULT_VALUE = 0
+
+        var res = create2DArray(rows: Int(rows), cols: Int(columns), initialValue: DEFAULT_VALUE)
+        let storedObstacles = self.value(forKey: "obstacles") as! [StoredObstacle]
+
+        for obstacle in storedObstacles {
+            let col = Int(obstacle.positionX)
+            let row = Int(obstacle.positionY)
+
+            res[row][col] = Int(obstacle.type!)!
+        }
+
+        return res
+    }
+
+    private func create2DArray(rows: Int, cols: Int, initialValue: Int) -> [[Int]] {
+        return [[Int]](repeating: [Int](repeating: initialValue, count: rows), count: cols)
     }
 
 }
