ab15b0690383862c9044c88edf4499700872f5c8
Bili Xu <xubili93@gmail.com>
2017-03-28 15:37:07 +0800

    Implement LoadableGame protocol and extend StoredGame

---
+++ WoofRunner/Utilities/Storage/StoredGame+LoadableGame.swift
@@ -0,0 +1,21 @@
+//
+//  StoredGame+LoadableGame.swift
+//  WoofRunner
+//
+//  Created by Xu Bili on 3/28/17.
+//  Copyright © 2017 WoofRunner. All rights reserved.
+//
+
+extension StoredGame: LoadableGame {
+
+    /// Returns the stored platforms as a 2D array.
+    /// - Returns: 2D array of platform types
+    public func getPlatforms() -> [[Int]] {
+    }
+
+    /// Returns the stored obstacles of a game as a 2D array.
+    /// - Returns: 2D array of obstacle types
+    public func getObstacles() -> [[Int]] {
+    }
+
+}
