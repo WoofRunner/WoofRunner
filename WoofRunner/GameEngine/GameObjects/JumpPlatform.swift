0b069e451a5101c7f8759aa21b1a0eb77fae2c3a
Ta Eu <taeu93@gmail.com>
2017-03-31 00:33:58 +0800

    added jumpPlatform

---
+++ WoofRunner/GameEngine/GameObjects/JumpPlatform.swift
@@ -0,0 +1,24 @@
+//
+//  JumpFloor.swift
+//  WoofRunner
+//
+//  Created by limte on 31/3/17.
+//  Copyright © 2017 WoofRunner. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+
+class JumpPlatform : Platform {
+    
+    override init(_ pos: SCNVector3) {
+        super.init(pos)
+        tileType = TileType.floorLight
+        loadModel(tileType.getModelPath())
+    }
+    
+    convenience init() {
+        self.init(SCNVector3(0, 0, 0))
+    }
+}
