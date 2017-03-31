4a91386a7f5b88be16069846d7d1f9007fbf87ee
Ta Eu <taeu93@gmail.com>
2017-03-31 14:43:22 +0800

    poolManagerDelegate

---
+++ WoofRunner/GameEngine/GameObjects/PoolManagerDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  PoolManagerDelegate.swift
+//  WoofRunner
+//
+//  Created by limte on 31/3/17.
+//  Copyright © 2017 WoofRunner. All rights reserved.
+//
+
+protocol PoolManagerDelegate {
+    func poolTile(_ tile: Tile)
+}
