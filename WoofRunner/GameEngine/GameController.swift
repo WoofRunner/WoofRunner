196d423dca8b3ce00bcfe66359943f4603759867
Ta Eu <taeu93@gmail.com>
2017-04-06 22:03:32 +0800

    no message

--- WoofRunner/GameEngine/GameController.swift
+++ WoofRunner/GameEngine/GameController.swift
@@ -132,9 +132,9 @@ class GameController: UIViewController, PlayerDelegate, TileManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tempSetup()
+        //tempSetup()
         
-        /*
+        
         guard let uuid = gameUUID else {
             fatalError("Game UUID not defined")
         }
@@ -146,7 +146,7 @@ class GameController: UIViewController, PlayerDelegate, TileManagerDelegate {
             .onFailure { error in
                 print("\(error.localizedDescription)")
         }
-*/
+
     }
     
     override func didReceiveMemoryWarning() {
