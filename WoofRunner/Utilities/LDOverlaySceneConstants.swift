c0bdec2e8cecca7437cc20bf0cf536114b3919e4
See Loo Jane <muigek34@yahoo.com.sg>
2017-04-11 18:48:20 +0800

    Refactor out PaletteConstants to a new LDOverlayConstants file

---
+++ WoofRunner/Utilities/LDOverlaySceneConstants.swift
@@ -0,0 +1,38 @@
+//
+//  LDOverlaySceneConstants.swift
+//  WoofRunner
+//
+//  Created by See Loo Jane on 11/4/17.
+//  Copyright © 2017 WoofRunner. All rights reserved.
+//
+
+import SpriteKit
+
+struct LDOverlaySceneConstants {
+	
+	struct PaletteConstants {
+		
+		static let buttonTypesArray: [PaletteFunctionType] = [.platform, .obstacle, .delete]
+		static let numberOfPaletteButtons = CGFloat(buttonTypesArray.count)
+		
+		// Palette Buttons
+		static let paletteButtonWidth = CGFloat(60)
+		static let paletteButtonHeight = CGFloat(60)
+		static let paletteButtonSize = CGSize(width: paletteButtonWidth, height: paletteButtonHeight)
+		static let paletteButtonMargin = CGFloat(15)
+		
+		// Palette Menu
+		static let paletteWidth = paletteButtonWidth + (2 * paletteButtonMargin)
+		static let paletteHeight = (paletteButtonWidth * numberOfPaletteButtons) + ((numberOfPaletteButtons + 1) * paletteButtonMargin)
+		
+		// The bottom-left corner of the palette's frame
+		static let palettePosition = CGPoint(x: 45, y: 680)
+		
+		static let paletteButtonX0 = paletteWidth/2 // y-coord for the topmost palette button
+		static let paletteButtonY0 = paletteHeight - (paletteButtonWidth/2 + paletteButtonMargin) // x-coord for the topmost palette button
+		
+		static let paletteBackgroundColor = SKColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+	}
+	
+	
+}
--- WoofRunner/View/LevelDesigner/PaletteMenu/PaletteMenu.swift
+++ WoofRunner/View/LevelDesigner/PaletteMenu/PaletteMenu.swift
@@ -8,35 +8,15 @@
 
 import SpriteKit
 
-struct PaletteConstants {
-	static let buttonTypesArray: [PaletteFunctionType] = [.platform, .obstacle, .delete]
-	static let numberOfPaletteButtons = CGFloat(buttonTypesArray.count)
-	static let paletteButtonWidth = CGFloat(60)
-	static let paletteButtonHeight = CGFloat(60)
-	static let paletteButtonSize = CGSize(width: paletteButtonWidth, height: paletteButtonHeight)
-	static let paletteButtonMargin = CGFloat(15)
-	
-	static let paletteWidth = paletteButtonWidth + (2 * paletteButtonMargin)
-	static let paletteHeight = (paletteButtonWidth * numberOfPaletteButtons) + ((numberOfPaletteButtons + 1) * paletteButtonMargin)
-	
-	// The bottom-left corner of the palette's frame
-	static let palettePosition = CGPoint(x: 45, y: 680)
-	
-	static let paletteButtonX0 = paletteWidth/2 // y-coord for the topmost palette button
-	static let paletteButtonY0 = paletteHeight - (paletteButtonWidth/2 + paletteButtonMargin) // x-coord for the topmost palette button
-	
-	static let paletteBackgroundColor = SKColor(red: 0, green: 0, blue: 0, alpha: 0.8)
-}
-
-
 class PaletteMenu: SKNode {
 	
-	private let backgroundNode = CustomShapeNodes.getRoundedRectangleNode(height: PaletteConstants.paletteHeight,
-		                                                             width: PaletteConstants.paletteWidth,
+	typealias Palette = LDOverlaySceneConstants.PaletteConstants
+	
+	private let backgroundNode = CustomShapeNodes.getRoundedRectangleNode(height: Palette.paletteHeight,
+		                                                             width: Palette.paletteWidth,
 		                                                             radius: 25,
-		                                                             backgroundColor: PaletteConstants.paletteBackgroundColor)
+		                                                             backgroundColor: Palette.paletteBackgroundColor)
 	private var buttonArray = [PaletteButton]()
-	//var buttonTypesArray = [PaletteFunctionType]()
 	
 	override init() {
 		super.init()
@@ -54,25 +34,25 @@ class PaletteMenu: SKNode {
 	// of the menu's frame)
 	public func renderPaletteMenu() {
 		
-		self.position = PaletteConstants.palettePosition
+		self.position = Palette.palettePosition
 		
 		// Base x and y positions for the palette buttons
-		var buttonY = PaletteConstants.paletteButtonY0
-		let buttonX = PaletteConstants.paletteButtonX0
+		var buttonY = Palette.paletteButtonY0
+		let buttonX = Palette.paletteButtonX0
 		
 		// Attach background first
 		self.addChild(backgroundNode)
 		
 		// Create and attach the palette button nodes
-		for type in PaletteConstants.buttonTypesArray {
+		for type in Palette.buttonTypesArray {
 			
 			// Create
 			let buttonImage = type.getSpriteImageName()
-			let buttonNode = PaletteButton(imageNamed: buttonImage, funcType: type, size: PaletteConstants.paletteButtonSize)
+			let buttonNode = PaletteButton(imageNamed: buttonImage, funcType: type, size: Palette.paletteButtonSize)
 			
 			// Set position according to offsets
 			buttonNode.position = CGPoint(x: buttonX, y: buttonY)
-			buttonY -= PaletteConstants.paletteButtonWidth + PaletteConstants.paletteButtonMargin
+			buttonY -= Palette.paletteButtonWidth + Palette.paletteButtonMargin
 			
 			// Add node
 			self.addChild(buttonNode)
